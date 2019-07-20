7a36026f2f510fdebf920d1e9c82471c658d773f
Manzell <manzellhatten@gmail.com>
2019-07-19 20:34:10 -0400

    Added the navbar to the homepage will build out other bootstrap items later

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./style.css"
+
+function Nav() {
+    return (
+        <nav className="navbar navbar-light navbar-expand-lg" style={{ backgroundColor: "#1E5631" }}>
+            <a className="navbar-brand" href="/">Timber Creek</a>
+
+            <div className="collapse navbar-collapse">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">Leasing Profile</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">Payments</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav;
