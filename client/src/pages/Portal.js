57a0df0cf960c497d699edddd315809b084d335f
Manzell <manzellhatten@gmail.com>
2019-07-20 12:36:03 -0400

    Imported the grid system going to add the payment and view leasing profile pages

---
+++ client/src/pages/Portal.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Col, Row, Container } from "../components/Grid";
+
+class Portal extends Component {
+    state = {
+        rentPayment = 0
+    }
+
+    render() {
+
+    }
+}
+
+export default Portal;
