0cb28ca0d9669e967938029eb0c2e720e44d1e99
Manzell <manzellhatten@gmail.com>
2019-07-26 13:35:44 -0400

    added the routes for the login and fetching the profile information

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,7 +5,7 @@ const db = require("../models");
 
 process.env.SECRET_KEY = 'secret'
 
-module.exports = function(app) {
+module.exports = function (app) {
     app.post("/register", (req, res) => {
         const newUser = {
             firstName: req.body.firstName,
@@ -20,32 +20,70 @@ module.exports = function(app) {
                 email: req.body.email
             }
         })
-        .then(user => {
-            if (!user) {
-                const hash = bcrypt.hashSync(newUser.password, 10)
-                newUser.password = hash
-                
-                db.apartmentPortal.create(
-                    newUser
-                )
-                .then(currentUser => {
+            .then(user => {
+                if (!user) {
+                    const hash = bcrypt.hashSync(newUser.password, 10)
+                    newUser.password = hash
+
+                    db.apartmentPortal.create(newUser)
+                        .then(currentUser => {
+                            let token = jwt.sign(currentUser.dataValues, process.env.SECRET_KEY, {
+                                expiresIn: 1440
+                            });
+
+                            res.json({ token: token })
+                        })
+                        .catch(err => {
+                            res.send('error: ' + err)
+                        })
+                } else {
+                    res.json({ error: "User already exists" })
+                }
+            })
+            .catch(err => {
+                res.send("error: " + err)
+            })
+    })
+
+    app.post("/login", (req, res) => {
+        db.apartmentPortal.findOne({
+            where: {
+                email: req.body.email
+            }
+        })
+            .then(currentUser => {
+                if (bcrypt.compareSync(req.body.password, currentUser.password)) {
                     let token = jwt.sign(currentUser.dataValues, process.env.SECRET_KEY, {
                         expiresIn: 1440
                     });
 
                     res.json({ token: token })
-                })
-                .catch(err => {
-                    res.send('error: ' + err)
-                })
-            } else {
-                res.json({ error: "User already exists"})
-            }
-        })
-        .catch(err => {
-            res.send("error: " + err)
-        })
+                } else {
+                    res.send("User does not exist")
+                }
+            })
+            .catch(err => {
+                res.send('error: ' + err)
+            })
     })
 
-    
+    app.get("/profile" , (req, res) => {
+        const decoded = jwt.verify(req.headers['authorization'], process.env.SECRET_KEY)
+
+        db.apartmentPortal.findOne({
+            where: {
+                id: decoded.id
+            }
+        })
+            .then(currentUser => {
+                if (currentUser) {
+                    res.json(currentUser)
+                } else {
+                    res.send("User does not exist")
+                }
+            })
+            .catch(err => {
+                res.send("error: " + err)
+            })
+    })
 }
